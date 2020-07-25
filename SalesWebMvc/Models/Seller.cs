01dbc1182584b92ce4da560a57e4261c51384b8c
Rodrigo Vieira <rodrigovieirablack@gmail.com>
2020-07-24 22:21:51 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,17 +8,27 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} Required")] //Campo obrigatorio
+        [StringLength(60, MinimumLength =3, ErrorMessage = "{0} size shoul be between {2} and {1}")] //Tamanho 
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} Required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name= "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
         [Display (Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0,5000.0, ErrorMessage ="{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
         public Department Department { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
         public int DepartmentId { get; set; }
+        
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
